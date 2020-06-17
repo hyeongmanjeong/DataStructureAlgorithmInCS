23427d30853fde092c0549f2ebe43925e506912c
DESKTOP-E8P0TL5\jeong <hyeongman88@gmail.com>
2020-06-18 07:44:06 +0900

    Bubble 소팅 알고리듬 완료.

--- Sorting_Algorithm/Program.cs
+++ Sorting_Algorithm/Program.cs
@@ -11,15 +11,41 @@ namespace Sorting_Algorithm
         static void Main(string[] args)
         {
             int[] arr = new int[] { 10, 7, 1, 6, 8, 9, 2, 4 ,5,3};
-
+            //  before sorting array
+            PrintAllData(arr);
+            Console.WriteLine(" ------------------------- ");
+            Bubble_Sorting(arr);
+            Console.WriteLine(" ------------------------- ");
+            // after sorting array
+            PrintAllData(arr);
         }
         private static void Swap(ref int a, ref int b)
         {
-
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        private static void PrintAllData(int[] arr)
+        {
+            foreach (int n in arr)
+            {
+                Console.Write($"{n} -> ");
+            }
+            Console.WriteLine();
         }
         private static void Bubble_Sorting(int[] arr)
         {
-            
+            for (int i = arr.Length - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        Swap(ref arr[j],ref arr[j + 1]);
+                        PrintAllData(arr);
+                    }
+                }
+            }
         }
         private static void Selection_Sorting(int[] arr)
         {
