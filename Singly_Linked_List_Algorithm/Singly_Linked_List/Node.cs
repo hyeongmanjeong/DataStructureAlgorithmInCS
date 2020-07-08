c1b34dbeda4b0b1ae9e08a4f82c9938d53ff4064
DESKTOP-4KEJQ0R\jeong <hyeongman88@gmail.com>
2020-07-09 07:51:59 +0900

    Singly Linked List 작업 중.

---
+++ Singly_Linked_List_Algorithm/Singly_Linked_List/Node.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+
+namespace Singly_Linked_List
+{
+    public class Node
+    {
+        public int data { get; set; }
+        public Node next { get; set; }
+        public Node(int _data)
+        {
+            data = _data;
+        }
+    }
+}
--- Singly_Linked_List_Algorithm/Singly_Linked_List/Program.cs
+++ Singly_Linked_List_Algorithm/Singly_Linked_List/Program.cs
@@ -2,19 +2,6 @@
 
 namespace Singly_Linked_List
 {
-    public class SinglyLinkedList
-    {
-
-    }
-    public class Node
-    {
-        public int data { get; set; }
-        public Node next { get; set; }
-        public Node(int _data)
-        {
-            data = _data;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
