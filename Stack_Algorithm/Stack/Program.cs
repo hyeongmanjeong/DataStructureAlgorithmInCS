c2f67a28abb8336dab6a9d0df7bbbbba7c16806a
crosbyjeong <hyeongman88@gmail.com>
2020-08-24 11:38:31 +0900

    Stack  알고리듬 완성

--- Stack_Algorithm/Stack/Program.cs
+++ Stack_Algorithm/Stack/Program.cs
@@ -4,9 +4,41 @@ namespace Stack
 {
     class Program
     {
+        private static int[] stack = new int[10];
+        private static int top = -1;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Push(200);
+            Push(900);
+            Push(223);
+            PrintStackArray();
+            Push(200);
+            PrintStackArray();
+            Pop();
+            Pop();
+            PrintStackArray();
+        }
+        private static bool IsEmpty()
+        {
+            return top == -1;
+        }
+        private static void Push(int data)
+        {
+            top++;
+            stack[top] = data;
+        }
+        private static void Pop()
+        {
+            top--;
+        }
+        private static void PrintStackArray()
+        {
+            Console.WriteLine(" -------- Stack ---------");
+            for (int i = 0; i < top + 1; i++)
+            {
+                Console.Write($" {stack[i]} -> ");
+            }
+            Console.WriteLine();
         }
     }
 }
