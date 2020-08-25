7729b0608b86f67330346a92438169ca284f2342
DESKTOP-U5NUFT0\jeong <hyeongman88@gmail.com>
2020-08-25 09:54:10 +0900

    Queue 알고리듬 추가 완료

--- Queues_Algorithm/Queue/Program.cs
+++ Queues_Algorithm/Queue/Program.cs
@@ -4,9 +4,41 @@ namespace Queue
 {
     class Program
     {
+        private static int[] queue = new int[50];
+        private static int front = 0;
+        private static int rear = 0;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Push(10);
+            Push(18);
+            Push(20);
+            Push(33);
+            PrintQueueArray();
+            Pop();
+            Pop();
+            PrintQueueArray();
+        }
+        private static void PrintQueueArray()
+        {
+            Console.WriteLine(" ----- queue -----");
+            for(int i = front; i < rear; i++)
+            {
+                Console.Write($" {queue[i]} -> ");
+            }
+            Console.WriteLine();
+        }
+        private static void Push(int data)
+        {
+            queue[rear] = data;
+            rear++;
+        }
+        private static void Pop()
+        {
+            front++;
+        }
+        private static bool IsEmpty()
+        {
+            return front == rear;
         }
     }
 }
