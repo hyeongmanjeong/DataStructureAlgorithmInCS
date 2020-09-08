612a7fb4acf3a7109fb40ede05fa2bb06c9cc59a
DESKTOP-MFU311A\jeong <hyeongman88@gmail.com>
2020-09-08 11:52:49 +0900

    Linear Search 알고리듬 추가

--- Search_Algorithm/Linear_Search_Binary_Search/Program.cs
+++ Search_Algorithm/Linear_Search_Binary_Search/Program.cs
@@ -6,7 +6,22 @@ namespace Linear_Search_Binary_Search
     {
         static void Main(string[] args)
         {
-            
+            int[] arr = { 10, 4, 50, 9, 14, 17, 13 , 30,22,21 };
+
+            Console.WriteLine(LinearSearching(arr, 10));
+            Console.WriteLine(LinearSearching(arr, 99));
+
+        }
+        public static int LinearSearching(int[] arr,int value)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] == value)
+                { 
+                    return i;
+                }
+            }
+            return -1;
         }
     }
 }
